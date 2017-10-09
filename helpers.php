8114ab615a0fdab5457d12ddfbca86e7799ceabc
Gennadiy V Zadvornyi <gena@zadvornyi.com>
2017-10-10 00:42:37 +0600

    Add environment shorthand getter

--- helpers.php
+++ helpers.php
@@ -20,7 +20,9 @@ if (!function_exists('dd')) {
 	 * Dump & die
 	 */
 	function dd() {
+		echo '<pre>';
 		var_dump(func_get_args());
+		echo '</pre>';
 		die(1);
 	}
 
@@ -41,4 +43,15 @@ if (!function_exists('e')) {
 		return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
 	}
 
+}
+
+
+if (!function_exists('env')) {
+	function env($varname, $default = false){
+		$var = getenv($varname);
+		if($var === false){
+			return $default;
+		}
+		return $var;
+	}
 }
